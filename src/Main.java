05cc4205403d87f28a54406f6a05ef4aa730a81d
pLumbum82 <jobbsy@yandex.ru>
2022-09-08 17:23:53 +0500

    create method save steps

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,31 @@ public class Main {
         printMenu();
         int userInput = scanner.nextInt();
 
-        while (userInput != 0) {
+        while (true) {
             if (userInput == 1) {
-                System.out.println("вы выбрали 1");
+                System.out.println("Вы выбрали: Добавление кол-ва шагов за определенный день");
                 System.out.println("Введите месяц (начиная с нуля)");
                 int month = scanner.nextInt();
                 System.out.println("Введите день");
                 int day = scanner.nextInt();
                 System.out.println("Введите кол-во шагов");
                 int step = scanner.nextInt();
-                System.out.println(stepTracker.monthToData[month]);
+            boolean isSuccses = stepTracker.inStepDay(month, day-1, step);
+            if (isSuccses) {
+                System.out.println("Данные по шагам за месяц: " + month + ", день: " + day + " - записаны");
+            } else {
+                System.out.println("Данные за этот день уже есть");
+            }
+
             } else if (userInput == 2) {
-                System.out.println("вы выбрали 2");
+                System.out.println("Вы выбрали: Вывод статистика шагов за месяц");
+                //stepTracker.getMothStep
             } else if (userInput == 3) {
                 System.out.println("вы выбрали 3");
-            } else if (userInput > 3) {
+            } else if (userInput == 0) {
+                System.out.println("Программа завершена");
+                return;
+            } else {
                 System.out.println("Вы ввели несуществующее значение");
             }
 
@@ -30,10 +40,11 @@ public class Main {
             printMenu(); // печатаем меню ещё раз перед завершением предыдущего действия
             userInput = scanner.nextInt(); // повторное считывание данных от пользователя
         }
-        System.out.println("Программа завершена");
+
     }
 
     private static void printMenu() {
+        System.out.println("");
         System.out.println("Что вы хотите сделать? ");
         System.out.println("1 - Ввести количество шагов за определённый день");
         System.out.println("2 - Напечатать статистику за определённый месяц");
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,50 +1,70 @@
 public class StepTracker {
-
     MonthData[] monthToData;
-    void getMd(int month){
-        System.out.println(monthToData[month]);
-    }
+
+    int monthNum;
+    int daysNum;
+    int taregetDay;
+
+
     public StepTracker() {
-        monthToData = new MonthData[12];
+        monthNum = 12;
+        daysNum = 30;
+        taregetDay = 10000;
+
+        monthToData = new MonthData[monthNum];
         for (int i = 0; i < monthToData.length; i++) {
             monthToData[i] = new MonthData();
         }
     }
 
+    boolean inStepDay(int month, int day, int step) {
+        int stepWrite = monthToData[month].days[day];
+        if (stepWrite == 0) {
+            monthToData[month].days[day] = step;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     class MonthData {
-        // Заполните класс самостоятельно
-        int[] day = new int[30];
+        int[] days;
 
-        void monthDay() {
-            for (int i = 0; i < day.length; i++) {
-                System.out.print("День " + (i + 1) + ": " + day[i] + ", ");
+        MonthData() {
+            days = new int[daysNum];
+            for (int i = 0; i < days.length; i++) {
             }
-        }
-
-        void getMothStep() {
-            int monthStep = 0;
-            for (int i = 0; i < day.length; i++) {
-                monthStep = monthStep + day[i];
-            }
-            System.out.println("Всего шагов за месяц" + monthStep);
-        }
-
-        void getMaxStep() {
-            int maxStep = 0;
-            for (int i = 0; i < day.length; i++) {
-                if (day[i] > maxStep) {
-                    maxStep = day[i];
-                }
-            }
-            System.out.println("Максимальное кол-во шагов за месяц составило: " + maxStep);
-        }
-        void getMidStep(){
-            double midStep = 0;
-            for (int i = 0; i < day.length; i++) {
-                midStep = midStep + day[i];
-            }
-            midStep = midStep / day.length;
-            System.out.println("Срееднее кол-во шагов в месяце составило: " + midStep);
         }
     }
 }
+
+/*
+
+    void getMothStep() {
+        int monthStep = 0;
+        for (int i = 0; i < day.length; i++) {
+            monthStep = monthStep + day[i];
+        }
+        System.out.println("Всего шагов за месяц" + monthStep);
+    }
+
+    void getMaxStep() {
+        int maxStep = 0;
+        for (int i = 0; i < day.length; i++) {
+            if (day[i] > maxStep) {
+                maxStep = day[i];
+            }
+        }
+        System.out.println("Максимальное кол-во шагов за месяц составило: " + maxStep);
+    }
+
+    void getMidStep() {
+        double midStep = 0;
+        for (int i = 0; i < day.length; i++) {
+            midStep = midStep + day[i];
+        }
+        midStep = midStep / day.length;
+        System.out.println("Срееднее кол-во шагов в месяце составило: " + midStep);
+    }
+
+ */
